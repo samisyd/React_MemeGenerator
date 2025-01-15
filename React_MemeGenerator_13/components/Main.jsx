75fa1b56f35b9ae236d347e6755de78157470cca
samisyd <111076416+samisyd@users.noreply.github.com>
2025-01-14 16:23:50 -0800

    Meme generator developed in React

---
+++ React_MemeGenerator_13/components/Main.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react"
+import React from 'react'
+
+export default function Main() {
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    }) 
+   
+    
+    const [allMemes, setAllMemes] = useState([])
+
+    React.useEffect(() => {
+        fetch(`https://api.imgflip.com/get_memes`)
+            // parse the json body (that we get from api call )into javascript object
+            // and we can access that with data
+            .then(res => res.json())
+            .then(data => {
+                    // console.log(data.data )
+                    setAllMemes(data.data.memes)}
+                )
+        // console.log(data)
+    }, [])
+
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const newMemeUrl = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imageUrl: newMemeUrl
+        }))
+    }
+    
+    // WHERENER we use useEffect we can never make this a async function. So this doesnt work
+    // useEffect(async () => {
+    //     const res = await fetch("https://api.imgflip.com/get_memes")
+    //     const data = await res.json()
+    //     setAllMemes(data.data.memes)
+    // }, [])
+       
+    
+    function handleChange(event) {
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
+                    />
+                </label>
+                <button onClick={getMemeImage}>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
